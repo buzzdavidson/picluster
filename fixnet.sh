2c01f909af166a0b90eab3ae516b771bcea7213d
buzzdavidson <buzzdavidson@me.com>
2018-12-20 11:34:17 -0800

    Adding temporary script to set up gateway to NAT and masquerade traffic from pi

--- fixnet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo iptables -t nat -A POSTROUTING -o wlp6s0 -j MASQUERADE
-sudo iptables -A FORWARD -i eno1 -o wlp6s0 -j ACCEPT
-sudo iptables -A FORWARD -i wlp6s0 -o eno1 -m state --state RELATED,ESTABLISHED -j ACCEPT
